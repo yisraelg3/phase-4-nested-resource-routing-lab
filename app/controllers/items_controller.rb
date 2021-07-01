12efc5c4d252987e3f416661bab2f2a8de20be25
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-30 23:03:15 -0400

    commit tree 66919bb6d094e7c5b97b3a4d9481f645dd115ae4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,88 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  before_action :find_item, only:[:show]
+  before_action :find_user, only:[:create]
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      find_user
+      items = @user.items
+    else
+      items = Item.all
+    end
+      render json: items, include: :user
   end
 
+    def show
+      render json: @item, include: :user
+    end
+
+    def create
+      new_item = @user.items.create(item_params)
+      render json: new_item, include: :user, status: :created
+    end
+
+
+    private
+    def item_params
+      params.permit(:name, :description, :price, :user_id)
+    end
+    
+    def find_user
+      @user = User.find(params[:user_id])
+    end
+
+    def find_item
+      @item = Item.find(params[:id])
+    end
+
+    def record_not_found
+      render json: {error: "Item not found"}, status: :not_found
+    end
 end
+
+
+# class ItemsController < ApplicationController
+#   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+#   def index
+#     if params[:user_id]
+#       user = find_user
+#       items = user.items
+#     else
+#       items = Item.all
+#     end
+#     render json: items, include: :user
+#   end
+
+#   def show
+#     item = find_item
+#     render json: item
+#   end
+
+#   def create
+#     user = find_user
+#     item = user.items.create(item_params)
+#     render json: item, status: :created
+#   end
+
+#   private
+
+#   def find_item
+#     Item.find(params[:id])
+#   end
+
+#   def find_user
+#     User.find(params[:user_id])
+#   end
+
+#   def item_params
+#     params.permit(:name, :description, :price)
+#   end
+
+#   def render_not_found_response(exception)
+#     render json: { error: "#{exception.model} not found" }, status: :not_found
+#   end
+
+# end
